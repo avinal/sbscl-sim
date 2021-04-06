2be14a3b1305aef05299fe346dec43f44a13fd0f
Avinal Kumar <avinal.xlvii@gmail.com>
2021-04-06 14:25:28 +0530

    initial commit

---
+++ src/main/java/com/sbscl/sim/App.java
@@ -0,0 +1,13 @@
+package com.sbscl.sim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
