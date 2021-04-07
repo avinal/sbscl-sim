c73f5918e5ade927e15f1c27cb652b13f2951709
Avinal Kumar <avinal.xlvii@gmail.com>
2021-04-07 12:02:36 +0530

    add simulation workflow
    Signed-off-by: Avinal Kumar <avinal.xlvii@gmail.com>

---
+++ runner.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# git configuration
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "GitHub Actions"
+
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" # remote repo address
+
+# go to testfile directory
+cd testfiles || exit
+
+# download the latest jar
+curl https://github.com/avinal/sbscl-sim/releases/download/v1.0-SNAPSHOT/proto-sim-1.0-SNAPSHOT.jar --output proto-sim-1.0-SNAPSHOT.jar
+
+for testd in */ ; do
+    simargs=$(cat "${testd}/${testd}.txt")
+    java -jar proto-sim-1.0-SNAPSHOT.jar "${simargs}"
+    mkdir -p "${testd}/output"
+    mv -- *.svg *.xls output
+    git stage output/*
+    git commit -m "simulated ${testd}"
+done
+
+git remote add publisher "${remote_repo}"
+
+git checkout ${INPUT_BRANCH}
+
+# push to github
+git push publisher ${INPUT_BRANCH}
